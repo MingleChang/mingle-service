8ab65bc6d791f53f7accea68b83c540aeda505a5
Mingle Chang <mingle890805@gmail.com>
2024-02-15 17:23:03 +0800

    add cors

--- src/main/java/mingle/chang/service/config/WebConfig.java
+++ src/main/java/mingle/chang/service/config/WebConfig.java
@@ -24,7 +24,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowCredentials(true)
+                .allowCredentials(false)
                 .allowedHeaders("*")
                 .allowedMethods("*")
                 .allowedOrigins("*")
